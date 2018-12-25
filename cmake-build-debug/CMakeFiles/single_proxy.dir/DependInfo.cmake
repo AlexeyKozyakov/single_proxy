60fe31eaadfbda4d99d08f329c701d9169d6c4c2
AlexeyKozyakov <xelexerx@gmail.com>
2018-12-25 23:25:53 +0700

    fix POLLNVAl

---
+++ cmake-build-debug/CMakeFiles/single_proxy.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alex/projects/single_proxy/http_parser.c" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/http_parser.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/projects/single_proxy/CacheEntry.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/CacheEntry.cpp.o"
+  "/home/alex/projects/single_proxy/Client.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/Client.cpp.o"
+  "/home/alex/projects/single_proxy/MyProxy.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/MyProxy.cpp.o"
+  "/home/alex/projects/single_proxy/Selector.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/Selector.cpp.o"
+  "/home/alex/projects/single_proxy/Server.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/Server.cpp.o"
+  "/home/alex/projects/single_proxy/main.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/main.cpp.o"
+  "/home/alex/projects/single_proxy/parser.cpp" "/home/alex/projects/single_proxy/cmake-build-debug/CMakeFiles/single_proxy.dir/parser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/single_proxy.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/single_proxy.dir/CacheEntry.cpp.o"
+  "CMakeFiles/single_proxy.dir/Client.cpp.o"
+  "CMakeFiles/single_proxy.dir/http_parser.c.o"
+  "CMakeFiles/single_proxy.dir/main.cpp.o"
+  "CMakeFiles/single_proxy.dir/MyProxy.cpp.o"
+  "CMakeFiles/single_proxy.dir/parser.cpp.o"
+  "CMakeFiles/single_proxy.dir/Selector.cpp.o"
+  "CMakeFiles/single_proxy.dir/Server.cpp.o"
+  "single_proxy.pdb"
+  "single_proxy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/single_proxy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
